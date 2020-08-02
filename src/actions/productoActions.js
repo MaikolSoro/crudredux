94a902fcd4287139f4d283961db3cfe818b41399
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-08-02 12:54:05 -0600

    Obteniendo los nuevos valores

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -10,7 +10,8 @@ import {
 	PRODUCTO_ELIMINAR_ERROR,
 	OBTENER_PRODUCTO_EDITAR,
 	PRODUCTO_EDITADO_EXITO,
-	PRODUCTO_EDITADO_ERROR
+	PRODUCTO_EDITADO_ERROR,
+	COMENZAR_EDICION_PRODUCTO
 } from '../types';
 
 import clienteAxios from '../config/axios';
@@ -143,5 +144,23 @@ export function obtenerProductoEditar(producto) {
 
 const obtenerProductoEditarAction = producto => ({
 	type: OBTENER_PRODUCTO_EDITAR,
+	payload: producto
+});
+
+// editar un registro en la api y el state
+export function editarProductoAction(producto){
+	return  async (dispatch) => {
+		dispatch( editarProducto(producto) );
+
+		try {
+			const resultado = await clienteAxios.put(`/productos/${producto.id}`, producto);
+		}catch(error) {
+
+		}
+	}
+}
+
+const editarProducto = producto =>({
+	type: COMENZAR_EDICION_PRODUCTO,
 	payload: producto
 });
--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { editarProductoAction } from '../actions/productoActions';
 const EditarProducto = () => {
 
+	// nuevo state de producto
+	const [producto, guardarProducto] = useState({
+		nombre: '',
+		precio: ''
+
+	});
 	// producto a editar
-	const producto = useSelector(state => state.productos.productoeditar);
-	if(!producto) return null;
-	
+	const productoeditar = useSelector(state => state.productos.productoeditar);
+
+	// llenar el state automaticamente
+	 useEffect(() => {
+		 guardarProducto(productoeditar);
+		 
+	 }, [productoeditar]);
+
+	 // leer  los datos del formulario 
+	 const onChangeFormulario = e => {
+		 guardarProducto({
+			 ...producto,
+			 [e.target.name] : e.target.value
+		 })
+	 }
 	const { nombre, precio, id } = producto;
+
+	const submitEditarProducto = e => {
+		e.preventDefault();
+
+		editarProductoAction();
+	}
 	return ( 
 		<div className="row justify-content-center">
 			<div className="col-md-8">
@@ -16,7 +40,7 @@ const EditarProducto = () => {
 						<h2 className="text-center mb-4 font-weight-bold">
 							Editar Producto
 						</h2>
-						<form>
+						<form onSubmit={submitEditarProducto}>
 							<div className="form-group">
 								<label>Nombre Producto</label>
 								<input type="text"
@@ -24,6 +48,7 @@ const EditarProducto = () => {
 									   placeholder="Nombre Producto"
 									   name="nombre"
 									   value={nombre}
+									   onChange={onChangeFormulario}
 								 />
 							</div>
 							<div className="form-group">
@@ -33,6 +58,7 @@ const EditarProducto = () => {
 									   placeholder="Precio Producto"
 									   name="precio"
 									   value={precio}
+									   onChange={onChangeFormulario}
 								 />
 							</div>
 							<button 
